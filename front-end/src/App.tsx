0ebcf202a78a4bf5c040ce7508aff38bac62ce79
RubemAZ <raz.rubem@gmail.com>
2024-01-17 06:41:19 -0300

    add assets

--- front-end/src/App.tsx
+++ front-end/src/App.tsx
@@ -1,10 +1,11 @@
 import React from 'react';
 import './App.css';
+import Navbar from './components/navbar/navbar'
 
 function App() {
   return (
-    <div className="App">
-      
+    <div>
+      <Navbar/>
     </div>
   );
 }
--- front-end/src/components/navbar/navbar.tsx
+++ front-end/src/components/navbar/navbar.tsx
@@ -1,8 +1,9 @@
 import React from "react";
+import './navbar.css'
 
 const Navbar = () => {
     return (
-        <div>
+        <div className='navbar'>
 
         </div>
     )
