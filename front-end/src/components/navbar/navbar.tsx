4da3a071b7ee8c208a79fba815ed63eec22dcfbe
RubemAZ <raz.rubem@gmail.com>
2024-01-18 21:09:52 -0300

    component navbar update

--- front-end/src/components/navbar/navbar.tsx
+++ front-end/src/components/navbar/navbar.tsx
@@ -1,10 +1,29 @@
-import React from "react";
+import React, { useState } from "react";
 import './navbar.css'
+import logo from '../assets/logo.png'
+import cart_icon from '../assets/cart_icon.png'
 
 const Navbar = () => {
+    const [menu, setMenu] = useState('shop')
     return (
         <div className='navbar'>
+            <div className="nav-logo">
+                <img src={logo} alt="" />
+                <h1>SHOPPER</h1>
+            </div>
+            
+            <ul className="nav-menu">
+                <li onClick={()=>{setMenu('shop')}}>Shop {menu==='shop'?<hr/>:<></>}</li>
+                <li onClick={()=>{setMenu('mens')}}>Mens {menu==='mens'?<hr/>:<></>}</li>
+                <li onClick={()=>{setMenu('womens')}}>Womens {menu==='womens'?<hr/>:<></>}</li>
+                <li onClick={()=>{setMenu('kids')}}>Kids {menu==='kids'?<hr/>:<></>}</li>
+            </ul>
 
+            <div className="nav-login-cart">
+                <button>Login</button>
+                <img src={cart_icon} alt="" />
+                <div className="nav-cart-count">0</div>
+            </div>
         </div>
     )
 }
