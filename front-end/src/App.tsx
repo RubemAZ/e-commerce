bc2e1e75f7724c1a5144a012b8ef60197be31e95
RubemAZ <raz.rubem@gmail.com>
2024-01-23 22:36:17 -0300

    configure setup react routes

--- front-end/src/App.tsx
+++ front-end/src/App.tsx
@@ -1,11 +1,31 @@
-import React from 'react';
 import './App.css';
 import Navbar from './components/navbar/navbar'
+import { BrowserRouter, Routes, Route } from 'react-router-dom';
+import Shop from './pages/shop';
+import ShopCategory from './pages/shopCategory';
+import Product from './pages/product';
+import Cart from './pages/cart';
+import LoginSignup from './pages/login-signup';
 
-function App() {
+const App = () => {
   return (
     <div>
+      <BrowserRouter>
       <Navbar/>
+
+      <Routes>
+        <Route path='/' element={<Shop/>}/>
+        <Route path='/mens' element={<ShopCategory /*category="men"*/ />}/>
+        <Route path='/womens' element={<ShopCategory /*category="women"*/ />}/>
+        <Route path='/kids' element={<ShopCategory /*category="kid"*/ />}/>
+        <Route path='/product' element={<Product/>}>
+          <Route path=':productId' element={<Product/>}/>
+        </Route>
+        <Route path='/cart' element={<Cart/>}/>
+        <Route path='/login' element={<LoginSignup/>} />
+      </Routes>
+
+      </BrowserRouter>
     </div>
   );
 }
